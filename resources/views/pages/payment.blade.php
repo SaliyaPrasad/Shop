954690083cd1297477899137f0fa20ee5b9169d0
SaliyaPrasad <saliyaprasad2@gmail.com>
2019-06-06 11:38:27 +0530

    erros fix

--- resources/views/pages/payment.blade.php
+++ resources/views/pages/payment.blade.php
@@ -1,106 +1,93 @@
 @extends('layout')
 @section('content')
 
-<section id="cart_items">
-		<div class="container">
-			
-			<div class="table-responsive cart_info">
-				<?php
+
+<section id="do_action">
+	<div class="container">
+	<div class="col-md-12">
+	<?php
                      $contents=Cart::content();
 
 				?>
-				<table class="table table-condensed">
-					<thead>
-						<tr class="cart_menu">
-							<td class="image">Image</td>
-							<td class="description">Name</td>
-							<td class="price">Price</td>
-							<td class="quantity">Quantity</td>
-							<td class="total">Total</td>
-							<td>Action</td>
-						</tr>
-					</thead>
-					<tbody>
-						<?php foreach ($contents as $v_contents) {?>
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="{{URL::to($v_contents->options->image)}}" height="80px" width="80px" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">{{$v_contents->name}}</a></h4>
-								
-							</td>
-							<td class="cart_price">
-								<p>{{$v_contents->price}}</p>
-							</td>
-							<td class="cart_quantity">
-							<div class="cart_quantity_button">
-								<form action="{{url('/update-cart')}}" method="post">
-									{{ csrf_field()}}
-									<input class="cart_quantity_input" type="text" name="qty" value="{{$v_contents->qty}}" autocomplete="off" size="2">
-									<input  type="hidden" name="rowId" value="{{$v_contents->rowId}}"  >
-									<input type="submit" name="submit" value="update" class="btn btn-sm btn-default">
-								</form>
+						<div class="order-summary clearfix">
+							<div class="section-title">
+								<h3 class="title">Order Review</h3>
 							</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">{{$v_contents->total}}</p>
-							</td>
-							<td class="cart_delete">
+							<table class="shopping-cart-table table">
+								<thead>
+									<tr>
+										<th>Product</th>
+										<th></th>
+										<th class="text-center">Price</th>
+										<th class="text-center">Quantity</th>
+										<th class="text-center">Total</th>
+										<th class="text-right"></th>
+									</tr>
+								</thead>
+								<tbody>
+								<?php foreach ($contents as $v_contents) {?>
+									<tr>
+										<td class="thumb"><img src="{{URL::to($v_contents->options->image)}}" alt=""></td>
+										<td class="details">
+											<a href="#">{{$v_contents->name}}</a>
+											<ul>
+												<li><span>Size: XL</span></li>
+												<li><span>Color: Camelot</span></li>
+											</ul>
+										</td>
+										<td class="price text-center"><strong>${{$v_contents->price}}</strong><br><del class="font-weak"><small>$40.00</small></del></td>
+										<td class="qty text-center">
+										
+										<form action="{{url('/update-cart')}}" method="post">
+									{{ csrf_field()}}
+								
+										<input class="input" type="number" name="qty" value="{{$v_contents->qty}} ">
+										<input  type="hidden" name="rowId" value="{{$v_contents->rowId}}"  >
+									<input type="submit" name="submit" value="update" class="btn btn-sm btn-default"></form></td>
+										<td class="total text-center"><strong class="primary-color">${{$v_contents->total}}</strong></td>
+										<td class="text-right"><button class="main-btn icon-btn"><a href="{{('/delete-to-cart/'.$v_contents->rowId)}}" > <i class="fa fa-close"></i></button></td>
+									</tr>
+									
+								</tbody>
+								<tfoot>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>SUBTOTAL</th>
+										<th colspan="2" class="sub-total">${{$v_contents->total}}</th>
+									</tr>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>SHIPING</th>
+										<td colspan="2">Free Shipping</td>
+									</tr>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>TOTAL</th>
+										<th colspan="2" class="total">${{$v_contents->total}}</th>
+									</tr>
+								</tfoot>
+								<?php }?>
+							</table>
+							
+						</div>
 
-								<a class="cart_quantity_delete" href="{{URL::to('/delete-to-cart/'.$v_contents->rowId)}}"><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
-                       <?php }?>
-						
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</section> <!--/#cart_items-->
-<section id="do_action">
-	<div class="container">
-	
-		
-		<div class="paymentCont col-sm-8">
-					<div class="headingWrap">
-							<h3 class="headingTop text-center">Select Your Payment Method</h3>	
-							<p class="text-center">Created with bootsrap button and using radio button</p>
 					</div>
-<!-- 					
-					<div class="paymentWrap">
-					
-						<div class="btn-group paymentBtnGroup btn-group-justified" data-toggle="buttons">
-						<form action="{{url('/order-place')}}" method="post">
-							{{csrf_field()}}
-				            <label class="btn paymentMethod active">
-				            	<div class="method visa"></div>
-				                <input type="radio" name="payment_gateway" value="visa" checked> 
-				            </label>
-				            <label class="btn paymentMethod">
-				            	<div class="method master-card"></div>
-				                <input type="radio" name="payment_gateway" value="mastercard"> 
-				            </label>
-				            <label class="btn paymentMethod">
-			            		<div class="method amex"></div>
-				                <input type="radio" name="payment_gateway" value="amex">
-				            </label>
-				       
-				            <label class="btn paymentMethod">
-			            		<div class="method ez-cash"></div>
-				                <input type="radio" name="payment_gateway" value="ezcash"> 
-				            </label> 
-							<input type="submit" class="btn btn-success" value="Submit">
-				       		</form>
-				        </div>        
-					</div> -->
+		
+		 <div class="payments-methods">
+							<div class="section-title">
+								<h4 class="title">Payments Methods</h4>
+							</div>
 					<form action="{{url('/order-place')}}" method="post">
 					{{csrf_field()}}
 					<input type="radio" name="payment_method" value="visa">Credit Or Debit Card<br>
 					<input type="radio" name="payment_method" value="paypal">paypal<br>
 					<input type="radio" name="payment_method" value="ezcash">ezcash<br>
-					<input type="submit" name="" value="Done">
+					
+					<div class="pull-right">
+								<button class="primary-btn">Place Order</button>
+							</div>
 					</form>
+				</div>
 				</div>
 	</div>
 </section><!--/#do_action-->
